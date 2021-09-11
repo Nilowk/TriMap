c8ea961ee38c576612bb58cbb2c2d3ef19a3ae50
Nilowk <rayotantoine@gmail.com>
2021-09-11 09:06:25 +0200

    getSecondValue

--- TriMap.java
+++ TriMap.java
@@ -9,9 +9,14 @@ public class TriMap<K, V, X> {
     private List<V> values = new ArrayList<>();
     private List<X> secondValues = new ArrayList<>();
 
-    public X put(K key, V value, X secondValue) throws Exception {
+    public X put(K key, V value, X secondValue) {
         if (keys.contains(key)) {
-            throw new Exception();
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
         }
         this.keys.add(key);
         this.values.add(value);
@@ -25,6 +30,19 @@ public class TriMap<K, V, X> {
             for (K k : keys) {
                 if (k == key) {
                     return values.get(index);
+                }
+                index++;
+            }
+        }
+        return null;
+    }
+
+    public X getSecondValue(K key) {
+        if (this.keys.contains(key)) {
+            int index = 0;
+            for (K k : keys) {
+                if (k == key) {
+                    return secondValues.get(index);
                 }
                 index++;
             }
