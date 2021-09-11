5a0576ca7a1d5439f72ea0e70fbe44f05e2e1946
Nilowk <rayotantoine@gmail.com>
2021-09-11 09:39:28 +0200

    functions

--- TriMap.java
+++ TriMap.java
@@ -9,43 +9,76 @@ public class TriMap<K, V, X> {
     private List<V> values = new ArrayList<>();
     private List<X> secondValues = new ArrayList<>();
 
-    public X put(K key, V value, X secondValue) {
+    public void put(K key, V value, X secondValue) {
         if (keys.contains(key)) {
             try {
                 throw new Exception();
             } catch (Exception e) {
                 e.printStackTrace();
-                return null;
+                return;
             }
         }
         this.keys.add(key);
         this.values.add(value);
         this.secondValues.add(secondValue);
-        return secondValue;
+    }
+
+    public void replace(K key, V newValue) {
+        if (keys.contains(key)) {
+            int index = getIndex(key);
+            values.set(index, newValue);
+        }
+    }
+
+    public void replaceS(K key, X newSecondValue) {
+        if (keys.contains(key)) {
+            int index = getIndex(key);
+            secondValues.set(index, newSecondValue);
+        }
     }
 
     public V getValue(K key) {
         if (this.keys.contains(key)) {
-            int index = 0;
-            for (K k : keys) {
-                if (k == key) {
-                    return values.get(index);
-                }
-                index++;
-            }
+            int index = getIndex(key);
+            return values.get(index);
         }
         return null;
     }
 
     public X getSecondValue(K key) {
         if (this.keys.contains(key)) {
-            int index = 0;
-            for (K k : keys) {
-                if (k == key) {
-                    return secondValues.get(index);
-                }
-                index++;
+            int index = getIndex(key);
+            return secondValues.get(index);
+        }
+        return null;
+    }
+
+    public void remove(K key) {
+        if (keys.contains(key)) {
+            int index = getIndex(key);
+            keys.remove(key);
+            values.remove(index);
+            secondValues.remove(index);
+        }
+    }
+
+    public void clear() {
+        keys.clear();
+        values.clear();
+        secondValues.clear();
+    }
+
+    public int getSize() {
+        return keys.size();
+    }
+
+    private Integer getIndex(K key) {
+        int index = 0;
+        for (K k : keys) {
+            if (k == key) {
+                return index;
             }
+            index++;
         }
         return null;
     }
